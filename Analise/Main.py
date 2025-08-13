28ea124e94c1cf17d48569a58e11b0cdd70837f5
Allison de Paulo Sousa Avila <allissonsousa7@outlook.com>
2025-08-13 12:01:36 -0300

    Add initial project files and analysis draft
    Added PyCharm project configuration files, a draft for data analysis in Analise/Main.py, and the Dicionario_SIM_2025.pdf reference document. This sets up the project structure and provides initial notes for further development.

---
+++ Analise/Main.py
@@ -0,0 +1,16 @@
+"""Link para acessar os dados https://dados.gov.br/dados/conjuntos-dados/sim-1979-2019>> """
+
+import csv
+import os
+
+"""Rascunho da analise"""
+
+"""
+Levantamento da problematica
+Oque vai ser analisado?
+Qual a relevância dos dados?
+Levantamento da solução viável embazada nos dados
+Amostragem de dados
+Analise qualitativa por uso de chatbot
+
+"""
