7152249b7e7146b7852371733528fece616b0193
Allison de Paulo Sousa Avila <allissonsousa7@outlook.com>
2025-08-15 17:17:25 -0300

    extração de colunas especificas do xlsx e contagem de elemento, tambem plotagem de um grafico

--- Analise/Main.py
+++ Analise/Main.py
@@ -6,8 +6,33 @@ import math
 import csv
 
 
-dado = r'C:\Users\allisson.avila\Documents\GitHub\Health-Data-Py\Analise\Mortalidade_Geral_1982.csv'
+dado = r'C:\Users\allisson.avila\Documents\GitHub\Health-Data-Py\Analise\Mortalidade_Geral_1989.xlsx'
 
-df = pd.read_csv(dado)
+# LOCALIDADE --- btenção de contagem dos locail mais recorrentes de mortalidade
 
-print(df.head())
+arquivo = pd.read_excel(dado)
+contagem = arquivo['LOCALOBITO'].value_counts().reset_index()
+contagem.columns = ['Local do Obito', 'Quantidade']
+
+hospital = contagem.loc[contagem['Local do Obito'] == 1, 'Quantidade'].sum()
+domicilio = contagem.loc[contagem['Local do Obito'] == 3, 'Quantidade'].sum()
+publica = contagem.loc[contagem['Local do Obito'] == 4, 'Quantidade'].sum()
+outros = contagem.loc[contagem['Local do Obito'] == 0, 'Quantidade'].sum()
+
+nomes_barras = ["Hospital","Domicilio","outros","Publica"]
+valores = [hospital, domicilio,outros, publica]
+
+plt.figure(figsize = (6,6))
+barras = plt.bar(nomes_barras, valores,color='red')
+plt.xlabel('Local do Obito')
+plt.ylabel('Quantidade')
+plt.title('Quantidade de obitos por localidade em 1989')
+plt.xticks(contagem['Local do Obito'])
+
+
+for barra in barras:
+    altura = barra.get_height()
+    plt.text(barra.get_x() + barra.get_width()/2, altura + 1, str(altura), ha='center', fontsize=8)
+plt.show()
+
+generos = arquivo[]
