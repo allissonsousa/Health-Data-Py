3dcb310c23609fbee6def3792db3d8d068ddf9e8
Allison de Paulo Sousa Avila <allissonsousa7@outlook.com>
2025-08-21 14:25:53 -0300

    codigos para plotagem de graficos de barra e histograma
    Inclusao de  codigos para plotagem de graficos de barra e histograma referente  a faixa etaria

--- Analise/Main.py
+++ Analise/Main.py
@@ -6,11 +6,10 @@ import math
 import csv
 
 dado = r'C:\Users\allisson.avila\Documents\GitHub\Health-Data-Py\Analise\Mortalidade_Geral_1989.xlsx'
-
-# LOCALIDADE --- btenção de contagem dos locail mais recorrentes de mortalidade
+#--------------------------------------------------
+# LOCALIDADE --- obtenção de contagem dos locais mais recorrentes de mortalidade [hospital,via publica, outros]
 
 arquivo = pd.read_excel(dado)
-
 
 contagem = arquivo['LOCALOBITO'].value_counts().reset_index()
 contagem.columns = ['Local do Obito', 'Quantidade']
@@ -23,7 +22,7 @@ outros = contagem.loc[contagem['Local do Obito'] == 0, 'Quantidade'].sum()
 nomes_barras = ["Hospital","Domicilio","outros","Publica"]
 valores = [hospital, domicilio,outros, publica]
 #plotagem do grafico
-plt.figure(figsize = (6,6))
+plt.figure(figsize = (8,8))
 barras = plt.bar(nomes_barras, valores,color='red')
 plt.xlabel('Local do Obito')
 plt.ylabel('Quantidade')
@@ -35,8 +34,8 @@ for barra in barras:
     plt.text(barra.get_x() + barra.get_width()/2, altura + 1, str(altura), ha='center', fontsize=8)
 plt.show()
 
-
-
+#-----------------------------------------------
+#GENERO --- obtenção da contagem deos óbitos por sexo identificado
 
 Generos = arquivo['SEXO'].value_counts().reset_index()
 Generos.columns = ['SEXO', 'Quantidade']
@@ -47,12 +46,13 @@ masculino = Generos.loc[Generos['SEXO'] == 2, 'Quantidade'].sum()
 
 nomes_generos = ['Masculino','Feminino','Indefinido']
 valoresgeneros = [masculino, feminino, nulo]
+
 #plotagem do grafico
-plt.figure(figsize = (6,6))
+plt.figure(figsize = (8,8))
 genbarra = plt.bar(nomes_generos, valoresgeneros,color='blue')
 plt.xlabel('SEXO')
 plt.ylabel('Quantidade')
-plt.title('Quantidade de obitos por genero em 1989')
+plt.title('Quantidade de obitos por gênero em 1989')
 plt.xticks(Generos['SEXO'])
 for g in genbarra:
     altura = g.get_height()
@@ -60,5 +60,21 @@ for g in genbarra:
 
 plt.show()
 
+# --------------------------------------------
+#IDADE -- análise com base nas idades
+# --- Gráfico de Barras (para idades exatas) ---
+contaidade = arquivo['IDADE'].value_counts().sort_index()
+plt.figure(figsize=(10,10))
+plt.bar(contaidade.index, contaidade.values)
+plt.xlabel("Idade")
+plt.ylabel("Quantidade de pessoas")
+plt.title("Recorrência de idades")
+plt.show()
 
-
+# --- Histograma (faixas de idade) ---
+plt.figure(figsize=(10,5))
+plt.hist(arquivo['IDADE'], bins=10, edgecolor="black")  # bins controla o agrupamento
+plt.xlabel("Idade")
+plt.ylabel("Frequência")
+plt.title("Distribuição de idades")
+plt.show()
