7d2493e86215f068a03292a8926a1a6068be3628
Allison de Paulo Sousa Avila <allissonsousa7@outlook.com>
2025-08-25 16:59:32 -0300

    Update APIREQUEST.py

--- Analise/APIREQUEST.py
+++ Analise/APIREQUEST.py
@@ -6,7 +6,7 @@ url = "http://apidadosabertos.saude.gov.br/vigilancia-e-meio-ambiente/sistema-de
 
 #parametros da requisição
 params = {
-    'limit' : 100,
+    'limit' : 1000,
     'offset' : 1
 }
 
@@ -17,18 +17,20 @@ response = requests.get(url, params=params, headers={'accept': 'application/json
 if response.status_code == 200:
     df = pd.DataFrame(response.json()) #transforma em Dataframe do pandas
     df_2020 = df
-    print(df_2020.head())
+    print(df_2020.shape)
 else:
     print(f"Erro ao consultar dados{response.status_code}")
 
 
+
 # PEGANDO TODOS OS DADOS DA API
 todos_dados = []
-for i in range(0, 20): #vou pegar só 10 paginas como exemplo pra nao sobrecarregar durante o teste
+for i in range(0, 4): #vou pegar só 10 paginas como exemplo pra nao sobrecarregar durante o teste
     params = {'limit' : 1000, 'offset' : i}
     r = requests.get(url, params=params, headers={'accept': 'application/json'})
     if r.status_code == 200:
         todos_dados.extend(r.json())
+        print(f"todos dados foram para o dataframe {i+1}")
+    else:
+        print(f"Erro ao consultar dados{r.status_code}")
 
-df = pd.DataFrame(todos_dados)
-print(df.head())
